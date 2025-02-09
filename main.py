76dbc145b28f91fa3e14665759db4cd6d00fc399
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2025-02-09 17:42:53 +0600

    Final Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-USERNAME = "naimul"
-TOKEN = "123456789"
+USERNAME = "name"
+TOKEN = "password"
 GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
